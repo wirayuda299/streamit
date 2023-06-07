2291db72eb5a2273bc959d575360e20fecdd50e2
wirayuda <wirayuda233@gmail.com>
2023-06-07 09:37:29 -0700

    add templog out btn

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react';
 import UserImage from './UserImage';
 import CreateButton from './CreateButton';
 import Notification from './Notification';
+import { signOut } from 'next-auth/react';
 type ProfileProps = {};
 
 const Profile: FC<ProfileProps> = (props) => {
@@ -10,6 +11,7 @@ const Profile: FC<ProfileProps> = (props) => {
 			<CreateButton />
 			<Notification />
 			<UserImage />
+			<button onClick={() => signOut()}>Log Out</button>
 		</div>
 	);
 };
